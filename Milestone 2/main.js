87a53372407e7e8de98e69daa3a44994280bdd6b
LOSAFABIO <fabio.losa95@gmail.com>
2022-02-17 18:06:58 +0100

    Tentativo di bonus andato a male

--- Milestone 2/main.js
+++ Milestone 2/main.js
@@ -1,16 +1,29 @@
 let myApp = new Vue ({
     el : '#app',
     data: {
-        myArr : []
+        myArr : [],
+        myArr2 : []
     },
     mounted : function(){
             axios.get(`http://localhost:8888/php-ajax-dischi/Milestone%202/api.php`)
             .then((risposta) => {
                 this.myArr = (risposta.data)
-                console.log(risposta);
+                console.log(this.myArr);
+            })
+            .catch(function (error) {
+                console.log(error);
+            }),
+            axios.get(`http://localhost:8888/php-ajax-dischi/Milestone%202/apiGenre.php`)
+            .then((risposta2) => {  
+                console.log(risposta2)
+                this.myArr2 = (risposta2.data[0].genre)
+                this.myArr2.slice(myArr2, 1)
+
+                console.log(this.myArr2.data);
             })
             .catch(function (error) {
                 console.log(error);
             })
+
     }
 })
